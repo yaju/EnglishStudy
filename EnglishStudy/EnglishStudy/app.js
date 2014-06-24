e80148a3ecd00114b89400795f3756f0fdfefd2f
yaju <hirapon@mtj.biglobe.ne.jp>
2014-06-25 01:22:26 +0900

    新規作成

---
+++ EnglishStudy/EnglishStudy/app.js
@@ -0,0 +1,24 @@
+﻿var StudyApp = (function () {
+    function StudyApp(element) {
+        this.obj = {
+            col001: { "Question": "これは教会です", "Answer": ["This", "is", "charch"], "Mark": "." },
+            col002: { "Question": "あれは机ですか", "Answer": ["Is", "that", "a", "desk"], "Mark": "?" }
+        };
+        //this.output = JSON.parse(this.obj);
+        this.element = element;
+        this.element.innerHTML += this.obj["col001"].Question;
+        for (var ans in this.obj["col001"].Answer) {
+            this.span = document.createElement('span');
+            this.element.appendChild(this.span);
+            this.span.innerText = this.obj["col001"].Answer[ans];
+        }
+    }
+    return StudyApp;
+})();
+
+window.onload = function () {
+    var el = document.getElementById('content');
+    var main = new StudyApp(el);
+    //greeter.start();
+};
+//# sourceMappingURL=app.js.map
---
+++ EnglishStudy/EnglishStudy/app.ts
@@ -0,0 +1,30 @@
+﻿class StudyApp {
+    element: HTMLElement;
+    span: HTMLElement;
+    timerToken: number;
+
+    output: JSON;
+    obj: any =
+    {
+        col001: { "Question": "これは教会です", "Answer": ["This", "is", "charch"], "Mark":"."},
+        col002: { "Question": "あれは机ですか", "Answer": ["Is", "that", "a", "desk"], "Mark": "?" },
+    };
+
+    constructor(element: HTMLElement) {
+        //this.output = JSON.parse(this.obj);
+
+        this.element = element;
+        this.element.innerHTML += this.obj["col001"].Question;
+        for (var ans in this.obj["col001"].Answer) {
+            this.span = document.createElement('span');
+            this.element.appendChild(this.span);
+            this.span.innerText = this.obj["col001"].Answer[ans];
+        }
+    }
+}
+
+window.onload = () => {
+    var el = document.getElementById('content');
+    var main = new StudyApp(el);
+    //greeter.start();
+};
