b409d92e7418a8cacd0d7c96d564e17f8046beda
yaju <hirapon@mtj.biglobe.ne.jp>
2014-06-30 01:09:10 +0900

    修正
    6/29修正分

--- EnglishStudy/EnglishStudy/app.js
+++ EnglishStudy/EnglishStudy/app.js
@@ -1,18 +1,51 @@
 ﻿var StudyApp = (function () {
     function StudyApp(element) {
+        var _this = this;
         this.obj = {
             col001: { "Question": "これは教会です", "Answer": ["This", "is", "charch"], "Mark": "." },
             col002: { "Question": "あれは机ですか", "Answer": ["Is", "that", "a", "desk"], "Mark": "?" }
         };
         //this.output = JSON.parse(this.obj);
-        this.element = element;
-        this.element.innerHTML += this.obj["col001"].Question;
+        var questionElm = document.getElementById('question');
+        questionElm.innerText += this.obj["col001"].Question;
+
+        //this.element = element;
+        //this.element.innerHTML += this.obj["col001"].Question;
+        var wordelm = document.getElementById('word');
+
         for (var ans in this.obj["col001"].Answer) {
             this.span = document.createElement('span');
-            this.element.appendChild(this.span);
+            wordelm.appendChild(this.span);
+            this.span.className = "wordOn";
             this.span.innerText = this.obj["col001"].Answer[ans];
+            this.span.onclick = function (event) {
+                return _this.onWordClick(_this, event);
+            };
         }
     }
+    StudyApp.prototype.onWordClick = function (that, e) {
+        var answerElm = document.getElementById('answer');
+        var span2 = document.createElement('span');
+        answerElm.appendChild(span2);
+        var target = e.target;
+        target.className = "wordOff";
+        span2.className = "answer";
+        span2.innerText = target.outerText;
+
+        //答え合わせ
+        var ans = "";
+        for (var i = 0; i < answerElm.childElementCount; i++) {
+            var elm = answerElm.childNodes[i];
+            ans += elm.outerText + " ";
+        }
+        var str = "";
+        for (var i in that.obj["col001"].Answer) {
+            str += that.obj["col001"].Answer[i] + " ";
+        }
+        if (ans.trim() == str.trim()) {
+            alert("Test");
+        }
+    };
     return StudyApp;
 })();
 
--- EnglishStudy/EnglishStudy/app.ts
+++ EnglishStudy/EnglishStudy/app.ts
@@ -1,6 +1,7 @@
 ﻿class StudyApp {
     element: HTMLElement;
     span: HTMLElement;
+    span2: HTMLElement;
     timerToken: number;
 
     output: JSON;
@@ -13,12 +14,46 @@
     constructor(element: HTMLElement) {
         //this.output = JSON.parse(this.obj);
 
-        this.element = element;
-        this.element.innerHTML += this.obj["col001"].Question;
+        var questionElm: HTMLElement = document.getElementById('question');
+        questionElm.innerText += this.obj["col001"].Question;
+
+
+        //this.element = element;
+        //this.element.innerHTML += this.obj["col001"].Question;
+
+        var wordelm: HTMLElement = document.getElementById('word');
+       
+
         for (var ans in this.obj["col001"].Answer) {
             this.span = document.createElement('span');
-            this.element.appendChild(this.span);
+            wordelm.appendChild(this.span);
+            this.span.className = "wordOn";
             this.span.innerText = this.obj["col001"].Answer[ans];
+            this.span.onclick = (event) => this.onWordClick(this, event);
+        }
+    }
+
+    onWordClick(that, e: MouseEvent) {
+        var answerElm: HTMLElement = document.getElementById('answer');
+        var span2 = document.createElement('span');
+        answerElm.appendChild(span2);
+        var target: HTMLElement = <HTMLElement>e.target;
+        target.className = "wordOff";
+        span2.className = "answer";
+        span2.innerText = target.outerText;
+
+        //答え合わせ
+        var ans: String = "";
+        for (var i = 0; i < answerElm.childElementCount; i++) {
+            var elm: HTMLElement = <HTMLElement>answerElm.childNodes[i];
+            ans += elm.outerText + " ";
+        }
+        var str = "";
+        for (var i in that.obj["col001"].Answer) {
+            str += that.obj["col001"].Answer[i] + " ";
+        }
+        if (ans.trim() == str.trim()) {
+            alert("Test");
         }
     }
 }
