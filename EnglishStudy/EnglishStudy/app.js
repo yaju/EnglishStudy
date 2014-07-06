3ffc2267f1b9e170dd2172ff43190f29fb271925
yaju <hirapon@mtj.biglobe.ne.jp>
2014-07-07 01:55:59 +0900

    修正
    2014/7/6修正

--- EnglishStudy/EnglishStudy/app.js
+++ EnglishStudy/EnglishStudy/app.js
@@ -1,50 +1,189 @@
 ﻿var StudyApp = (function () {
+    // コンストラクタ
     function StudyApp(element) {
-        var _this = this;
+        this.speed = 150;
+        this.timeInterval = 280;
+        this.countDown = 3 + 1;
         this.obj = {
             col001: { "Question": "これは教会です", "Answer": ["This", "is", "charch"], "Mark": "." },
             col002: { "Question": "あれは机ですか", "Answer": ["Is", "that", "a", "desk"], "Mark": "?" }
         };
-        //this.output = JSON.parse(this.obj);
+        // 問題表示
         var questionElm = document.getElementById('question');
         questionElm.innerText += this.obj["col001"].Question;
 
-        //this.element = element;
-        //this.element.innerHTML += this.obj["col001"].Question;
-        var wordelm = document.getElementById('word');
+        // 正答数(ダミーは除く)
+        this.answerCount = this.obj["col001"].Answer.length;
 
+        this.startAction();
+    }
+    // ワードクリック処理
+    StudyApp.prototype.onWordClick = function (that, e) {
+        var _this = this;
+        // 答えの位置に配置
+        var target = e.target;
+        var answerElm = document.getElementById('answer');
+
+        target.className = "wordOff";
+        var input = that.createWordBottun(that.zeroSuppress(target.id), "answer", target.value);
+        input.onclick = function (event) {
+            return _this.onWordCancelClick(_this, event);
+        };
+        answerElm.appendChild(input);
+
+        // 答え合わせ
+        if (answerElm.childElementCount == this.answerCount) {
+            // 全ての答え合わせ
+            if (this.checkingAnswers(answerElm)) {
+                alert("Test");
+            } else {
+                // 正解表示と正解と違う箇所の背景色を変更させる
+                this.missLastAction(answerElm);
+            }
+        } else {
+            // 正解と違う箇所をぷるぷるさせる
+            this.missAction(answerElm);
+        }
+    };
+
+    // ワードキャンセルクリック処理
+    StudyApp.prototype.onWordCancelClick = function (that, e) {
+        // 答えから対象ワードを削除する
+        var target = e.target;
+        var answerElm = document.getElementById('answer');
+        answerElm.removeChild(target);
+
+        // ワード一覧の対象ワードを戻す
+        var wordlistElm = document.getElementById('wordlist');
+        for (var i = 0; i < wordlistElm.childElementCount; i++) {
+            var elm = wordlistElm.childNodes[i];
+            if (elm.id == target.id) {
+                elm.className = "wordOn";
+                break;
+            }
+        }
+    };
+
+    // ゼロサプレス
+    StudyApp.prototype.zeroSuppress = function (val) {
+        return Number(val.replace(/\D/g, ''));
+    };
+
+    // ゼロパディング
+    StudyApp.prototype.zeroPadding = function (totalWidth, num) {
+        return ("00000" + num).slice(-totalWidth);
+    };
+
+    // ワードボタン生成
+    StudyApp.prototype.createWordBottun = function (no, clsName, val) {
+        var inputElm;
+
+        inputElm = document.createElement('input');
+        inputElm.type = "button";
+        inputElm.id = "btn" + this.zeroPadding(2, no);
+        inputElm.className = clsName;
+        inputElm.value = val;
+
+        return inputElm;
+    };
+
+    // ワードリスト生成
+    StudyApp.prototype.setWordList = function () {
+        var _this = this;
+        var wordlistElm = document.getElementById('wordlist');
+        var input;
+
+        wordlistElm.innerText = "";
+        wordlistElm.className = "wordStyle";
         for (var ans in this.obj["col001"].Answer) {
-            this.span = document.createElement('span');
-            wordelm.appendChild(this.span);
-            this.span.className = "wordOn";
-            this.span.innerText = this.obj["col001"].Answer[ans];
-            this.span.onclick = function (event) {
+            input = this.createWordBottun(ans, "wordOn", this.obj["col001"].Answer[ans]);
+            input.onclick = function (event) {
                 return _this.onWordClick(_this, event);
             };
+            wordlistElm.appendChild(input);
         }
-    }
-    StudyApp.prototype.onWordClick = function (that, e) {
-        var answerElm = document.getElementById('answer');
-        var span2 = document.createElement('span');
-        answerElm.appendChild(span2);
-        var target = e.target;
-        target.className = "wordOff";
-        span2.className = "answer";
-        span2.innerText = target.outerText;
+    };
 
-        //答え合わせ
+    // 答え合わせ処理
+    StudyApp.prototype.checkingAnswers = function (answerElm) {
         var ans = "";
+
         for (var i = 0; i < answerElm.childElementCount; i++) {
             var elm = answerElm.childNodes[i];
-            ans += elm.outerText + " ";
+            ans += elm.value + " ";
         }
         var str = "";
-        for (var i in that.obj["col001"].Answer) {
-            str += that.obj["col001"].Answer[i] + " ";
+        for (var i in this.obj["col001"].Answer) {
+            str += this.obj["col001"].Answer[i] + " ";
         }
         if (ans.trim() == str.trim()) {
-            alert("Test");
+            return true;
         }
+
+        return false;
+    };
+
+    // 不正解時のぷるぷる処理
+    StudyApp.prototype.missAction = function (answerElm) {
+        for (var i = 0; i < answerElm.childElementCount; i++) {
+            var elm = answerElm.childNodes[i];
+            if (elm.value != this.obj["col001"].Answer[i]) {
+                elm.className = "answer rum";
+            }
+        }
+    };
+
+    // 不正解時の最終処理
+    StudyApp.prototype.missLastAction = function (answerElm) {
+        var wordelm = document.getElementById('check');
+
+        var ptagElm = document.createElement('hr');
+        wordelm.appendChild(ptagElm);
+        var input;
+
+        var i = 0;
+        for (var ans in this.obj["col001"].Answer) {
+            input = this.createWordBottun(ans, "answer", this.obj["col001"].Answer[ans]);
+            wordelm.appendChild(input);
+
+            //違うところは色を変える
+            var elm = answerElm.childNodes[i];
+            if (elm.value != input.value) {
+                elm.className = "miss";
+            }
+            i++;
+        }
+    };
+
+    // カウントダウンアクション
+    StudyApp.prototype.startAction = function () {
+        var _this = this;
+        if (this.countDown > 1) {
+            this.countDown--;
+            this.timerToken = setTimeout(function () {
+                return _this.startAction();
+            }, 1000);
+        } else {
+            clearTimeout(this.timerToken);
+            this.setWordList();
+            this.timerAction();
+            return;
+        }
+        document.getElementById("wordlist").innerText = this.countDown + "";
+    };
+
+    // タイマーアクション
+    StudyApp.prototype.timerAction = function () {
+        var _this = this;
+        if (this.timeInterval > 0) {
+            this.timeInterval--;
+            this.timerToken = setTimeout(function () {
+                return _this.timerAction();
+            }, this.speed);
+        } else {
+            clearTimeout(this.timerToken);
+        }
+        document.getElementById("timer").style.width = this.timeInterval + "px";
     };
     return StudyApp;
 })();
--- EnglishStudy/EnglishStudy/app.ts
+++ EnglishStudy/EnglishStudy/app.ts
@@ -1,8 +1,10 @@
 ﻿class StudyApp {
     element: HTMLElement;
-    span: HTMLElement;
-    span2: HTMLElement;
     timerToken: number;
+    answerCount: number;
+    speed:number = 150;          // 速度
+    timeInterval: number = 280;
+    countDown:number = 3+1;
 
     output: JSON;
     obj: any =
@@ -11,50 +13,175 @@
         col002: { "Question": "あれは机ですか", "Answer": ["Is", "that", "a", "desk"], "Mark": "?" },
     };
 
+    // コンストラクタ
     constructor(element: HTMLElement) {
-        //this.output = JSON.parse(this.obj);
-
+        // 問題表示
         var questionElm: HTMLElement = document.getElementById('question');
         questionElm.innerText += this.obj["col001"].Question;
+        // 正答数(ダミーは除く)
+        this.answerCount = this.obj["col001"].Answer.length;
 
+        this.startAction();
+    }
 
-        //this.element = element;
-        //this.element.innerHTML += this.obj["col001"].Question;
+    // ワードクリック処理
+    onWordClick(that, e: MouseEvent) {
+        // 答えの位置に配置
+        var target: HTMLInputElement = <HTMLInputElement>e.target;
+        var answerElm: HTMLElement = document.getElementById('answer');
 
-        var wordelm: HTMLElement = document.getElementById('word');
-       
+        target.className = "wordOff";
+        var input: HTMLInputElement = that.createWordBottun(that.zeroSuppress(target.id), "answer", target.value);
+        input.onclick = (event) => this.onWordCancelClick(this, event);
+        answerElm.appendChild(input);
 
-        for (var ans in this.obj["col001"].Answer) {
-            this.span = document.createElement('span');
-            wordelm.appendChild(this.span);
-            this.span.className = "wordOn";
-            this.span.innerText = this.obj["col001"].Answer[ans];
-            this.span.onclick = (event) => this.onWordClick(this, event);
+        // 答え合わせ
+        if (answerElm.childElementCount == this.answerCount) {
+            // 全ての答え合わせ
+            if (this.checkingAnswers(answerElm)) {
+                alert("Test");
+            }
+            else {
+                // 正解表示と正解と違う箇所の背景色を変更させる
+                this.missLastAction(answerElm);
+            }
+        }
+        else {
+            // 正解と違う箇所をぷるぷるさせる
+            this.missAction(answerElm);
         }
     }
 
-    onWordClick(that, e: MouseEvent) {
+    // ワードキャンセルクリック処理
+    onWordCancelClick(that, e: MouseEvent) {
+        // 答えから対象ワードを削除する
+        var target: HTMLInputElement = <HTMLInputElement>e.target;
         var answerElm: HTMLElement = document.getElementById('answer');
-        var span2 = document.createElement('span');
-        answerElm.appendChild(span2);
-        var target: HTMLElement = <HTMLElement>e.target;
-        target.className = "wordOff";
-        span2.className = "answer";
-        span2.innerText = target.outerText;
+        answerElm.removeChild(target);
 
-        //答え合わせ
-        var ans: String = "";
+        // ワード一覧の対象ワードを戻す
+        var wordlistElm: HTMLElement = document.getElementById('wordlist');
+        for (var i = 0; i < wordlistElm.childElementCount; i++) {
+            var elm: HTMLInputElement = <HTMLInputElement>wordlistElm.childNodes[i];
+            if (elm.id == target.id) {
+                elm.className = "wordOn";
+                break;
+            }
+        }
+    }
+
+    // ゼロサプレス
+    zeroSuppress(val:string):number {
+        return Number(val.replace(/\D/g, ''));
+    }
+
+    // ゼロパディング
+    zeroPadding(totalWidth: number, num: number) {
+        return ("00000" + num).slice(-totalWidth);
+    }
+
+    // ワードボタン生成
+    createWordBottun(no: number, clsName: string, val: string): HTMLInputElement {
+        var inputElm: HTMLInputElement;
+
+        inputElm = document.createElement('input');
+        inputElm.type = "button";
+        inputElm.id = "btn" + this.zeroPadding(2, no);
+        inputElm.className = clsName;
+        inputElm.value = val;
+
+        return inputElm;
+    }
+
+    // ワードリスト生成
+    setWordList() {
+        var wordlistElm: HTMLElement = document.getElementById('wordlist');
+        var input: HTMLInputElement;
+
+        wordlistElm.innerText = "";
+        wordlistElm.className = "wordStyle";
+        for (var ans in this.obj["col001"].Answer) {
+            input = this.createWordBottun(ans, "wordOn", this.obj["col001"].Answer[ans]);
+            input.onclick = (event) => this.onWordClick(this, event);
+            wordlistElm.appendChild(input);
+        }
+    }
+
+    // 答え合わせ処理
+    checkingAnswers(answerElm: HTMLElement):boolean {
+        var ans: string = "";
+
         for (var i = 0; i < answerElm.childElementCount; i++) {
-            var elm: HTMLElement = <HTMLElement>answerElm.childNodes[i];
-            ans += elm.outerText + " ";
+            var elm: HTMLInputElement = <HTMLInputElement>answerElm.childNodes[i];
+            ans += elm.value + " ";
         }
         var str = "";
-        for (var i in that.obj["col001"].Answer) {
-            str += that.obj["col001"].Answer[i] + " ";
+        for (var i in this.obj["col001"].Answer) {
+            str += this.obj["col001"].Answer[i] + " ";
         }
         if (ans.trim() == str.trim()) {
-            alert("Test");
+            return true;
         }
+        
+        return false;
+    }
+
+    // 不正解時のぷるぷる処理
+    missAction(answerElm: HTMLElement) {
+        for (var i = 0; i < answerElm.childElementCount; i++) {
+            var elm: HTMLInputElement = <HTMLInputElement>answerElm.childNodes[i];
+            if (elm.value != this.obj["col001"].Answer[i]) {
+                elm.className = "answer rum";
+            }
+        }
+    }
+
+    // 不正解時の最終処理
+    missLastAction(answerElm: HTMLElement) {
+        var wordelm: HTMLElement = document.getElementById('check');
+
+        var ptagElm: HTMLElement = document.createElement('hr');
+        wordelm.appendChild(ptagElm);
+        var input: HTMLInputElement;
+
+        var i: number = 0;
+        for (var ans in this.obj["col001"].Answer) {
+            input = this.createWordBottun(ans, "answer", this.obj["col001"].Answer[ans]);
+            wordelm.appendChild(input);
+            //違うところは色を変える
+            var elm: HTMLInputElement = <HTMLInputElement>answerElm.childNodes[i];
+            if (elm.value != input.value) {
+                elm.className = "miss";
+            }
+            i++;
+        }
+    }
+
+    // カウントダウンアクション
+    startAction() {
+        if (this.countDown > 1) {
+            this.countDown--;
+            this.timerToken = setTimeout(() => this.startAction(), 1000);
+        }
+        else {
+            clearTimeout(this.timerToken);
+            this.setWordList();
+            this.timerAction();
+            return;
+        }
+        document.getElementById("wordlist").innerText = this.countDown + "";
+    }
+
+    // タイマーアクション
+    timerAction(){
+        if (this.timeInterval > 0) {
+            this.timeInterval--;
+            this.timerToken = setTimeout(() => this.timerAction(), this.speed);
+        }
+        else {
+            clearTimeout(this.timerToken);
+        }
+        document.getElementById("timer").style.width = this.timeInterval + "px";
     }
 }
 
